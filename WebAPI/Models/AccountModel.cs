fd2a74e34d7d9cff578be195776e27b0d9aca214
Yassine <yessin.bouayed@ensi-uma.tn>
2019-04-12 19:49:52 +0100

    test

--- WebAPI/Models/AccountModel.cs
+++ WebAPI/Models/AccountModel.cs
@@ -12,7 +12,7 @@ namespace WebAPI.Models
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        /test
         public string LoggedOn { get; set; }
     }
 }
